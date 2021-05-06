15c768227b05fb97e9fc339bf5caea683cef1849
Sean Melody <seandmelody@gmail.com>
2021-05-06 12:24:55 -0700

    levell functionality added

--- game.js
+++ game.js
@@ -6,6 +6,31 @@ context.canvas.height = 400;
 // width set to 1220 on the example
 context.canvas.width = 1120;
 
+
+// Obsticles
+let frameCount = 1;
+
+let obCount = frameCount;
+
+const obXCoors = []
+
+// For Each Level
+const nextFrame = () => {
+
+    // Level Up
+
+    frameCount++
+
+
+    for (let i = 0; i < obCount; i++) {
+
+        obXCoors = Math.floor(Math.random() * (1165 - 140 + 1) + 140)
+        obXCoors.push(obXCoors)
+    }
+}
+
+
+// Turn to skater
 const square = {
     height: 32,
     jumping: true,
@@ -76,17 +101,17 @@ const loop = function () {
 
     // Backdrop
 
-    context.fillStyle = "#201A21"
+    context.fillStyle = "#333333"
     context.fillRect(0, 0, 1220, 400)
 
-
+    // Player
     context.fillStyle = "#8DAA9D"; // hex for cube color
     context.beginPath();
     context.rect(square.x, square.y, square.width, square.height);
     context.fill();
 
     // Ground
-    context.fillStyle = "#2E2532"
+    context.strokeStyle = "#00000"
     context.lineWidth = 30;
     context.beginPath()
     context.moveTo(0, 385)
